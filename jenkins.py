07a24cab73afaba5b33cc0816cd2e72790698a5b
rishitanelluri <122783705+rishitanelluri@users.noreply.github.com>
2023-01-16 15:55:18 +0530

    Update jenkins.py

--- jenkins.py
+++ jenkins.py
@@ -1,1 +1,6 @@
-{"nbformat":4,"nbformat_minor":0,"metadata":{"colab":{"provenance":[],"authorship_tag":"ABX9TyPacqYJQWvCTxJ/tSJOj2Vd"},"kernelspec":{"name":"python3","display_name":"Python 3"},"language_info":{"name":"python"}},"cells":[{"cell_type":"code","execution_count":4,"metadata":{"colab":{"base_uri":"https://localhost:8080/"},"id":"YjqXK7sFwWUs","executionInfo":{"status":"ok","timestamp":1673858930919,"user_tz":-330,"elapsed":24,"user":{"displayName":"rishita nelluri","userId":"13123408164347565555"}},"outputId":"58a597bd-ef97-43f6-c632-abceeef50e5d"},"outputs":[{"output_type":"stream","name":"stdout","text":["4\n","16\n","36\n","64\n","100\n"]}],"source":["list1=[2,4,6,8,10]\n","for i in list1:\n","  print(i**2)\n","  \n"]},{"cell_type":"code","source":["\n","for i in list1:\n","  print(i**3)\n","  "],"metadata":{"colab":{"base_uri":"https://localhost:8080/"},"id":"l2oAVc2Zw1pm","executionInfo":{"status":"ok","timestamp":1673859066271,"user_tz":-330,"elapsed":437,"user":{"displayName":"rishita nelluri","userId":"13123408164347565555"}},"outputId":"de1f956b-ccd1-4d16-cf33-88819c0439da"},"execution_count":6,"outputs":[{"output_type":"stream","name":"stdout","text":["8\n","64\n","216\n","512\n","1000\n"]}]},{"cell_type":"code","source":[],"metadata":{"id":"vo5WOMnSxJ38"},"execution_count":null,"outputs":[]}]}
+list1=[2,4,6,8,10]
+for i in list1:
+  print(i**2)
+for i in list1:
+  print(i**3)
+  
